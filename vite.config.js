2b7ab669901ff2a1ce7dda579184bd1c0166403a
vitorGaia <vitorgaia0027@gmail.com>
2023-06-03 14:50:18 -0300

    Deploy test.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-gh-pages',
+  base: '/vitorGaia.github.io',
   plugins: [react()],
 })
