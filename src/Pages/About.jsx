d9bd754da8c1278ff58edf129abb05326ac16913
vitorGaia <vitorgaia0027@gmail.com>
2023-06-03 17:19:18 -0300

    Deploy test.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -22,7 +22,7 @@ function About() {
     <div className="about-page-container" id='about' ref={about}>
       <h2>Sobre Mim</h2>
 
-      <p>Estou em transição de carreira. Motivado pela minha paixão por tecnologia, desejo de adquirir novas habilidades e conhecimentos e a busca por uma carreira com maior potencial de crescimento e estabilidade financeira. Estou adquirindo novas habilidades através de cursos, treinamentos e estudos autônomos. Embora seja um desafio, estou determinado(a) a alcançar meus objetivos e construir uma carreira gratificante na área de tecnologia.</p>
+      <p>Estou em transição de carreira. Motivado pela minha paixão por tecnologia, desejo de adquirir novas habilidades/conhecimentos e a busca por uma carreira com maior potencial de crescimento e estabilidade financeira. Estou adquirindo novas habilidades através de cursos, treinamentos e estudos autônomos. Embora seja um desafio, estou determinado(a) a alcançar meus objetivos e construir uma carreira gratificante na área de tecnologia.</p>
 
       <TimeLine />
     </div>
