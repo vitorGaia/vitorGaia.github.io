753991c21d57d1b8dbe0f0bd57feef2f0f69d047
vitorGaia <vitorgaia0027@gmail.com>
2023-06-24 22:25:29 -0300

    feat: finaliza formatação da secao about desktop, no commit anterio era skills.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -29,9 +29,11 @@ function About() {
 
   return (
     <div className="about-page-container area-3-about" id='about' ref={about}>
-      <h2>Sobre Mim</h2>
+      <div className='about-titles-container'>
+        <h2>Sobre Mim</h2>
 
-      <p>Estou em transição de carreira. Motivado pela minha paixão por tecnologia, desejo de adquirir novas habilidades/conhecimentos e a busca por uma carreira com maior potencial de crescimento e estabilidade financeira. Estou adquirindo novas habilidades através de cursos, treinamentos e estudos autônomos. Embora seja um desafio, estou determinado(a) a alcançar meus objetivos e construir uma carreira gratificante na área de tecnologia.</p>
+        <p>Estou em transição de carreira. Motivado pela minha paixão por tecnologia, desejo de adquirir novas habilidades/conhecimentos e a busca por uma carreira com maior potencial de crescimento e estabilidade financeira. Estou adquirindo novas habilidades através de cursos, treinamentos e estudos autônomos. Embora seja um desafio, estou determinado(a) a alcançar meus objetivos e construir uma carreira gratificante na área de tecnologia.</p>
+      </div>
 
       <TimeLine />
     </div>
