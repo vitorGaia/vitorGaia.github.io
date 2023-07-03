8c7ab1e052a2e62472f745d7d853275efda77730
vitorGaia <vitorgaia0027@gmail.com>
2023-07-03 19:24:36 -0300

    feat: adiciona funcionalidade para receber email pelo form.

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -5,10 +5,16 @@ import '../Styles/Contacts.css';
 import { useContext, useEffect, useState } from 'react';
 import contactsBackground from '../Images/contactsBackground.gif';
 import AppContext from '../Contexts/AppContext';
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert';
 
 function Contacts() {
   const { formValues, setFormValues } = useContext(AppContext);
-  const [desableBtn, setDisablebtn] = useState(true);
+  const [disableBtn, setDisablebtn] = useState(true);
+
+  const SERVICE_ID = 'service_ak1civh';
+  const TEMPLATE_ID = 'template_g3kog9q';
+  const USER_ID = '5-w1CXGpaALYEp7ur';
 
   useEffect(() => {
     ScrollReveal().reveal('.area-5-contacts', {
@@ -38,9 +44,33 @@ function Contacts() {
     setFormValues({ ...formValues, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Lógica para enviar o formulário
+
+    const templateParams = {
+      from_name: formValues.nome,
+      from_email: formValues.email,
+      subject: formValues.assunto,
+      message: formValues.mensagem
+    };
+
+    try {
+      const response = await emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, USER_ID);
+      console.log('Email enviado com sucesso!', response);
+
+      setFormValues({
+        nome: '',
+        email: '',
+        assunto: '',
+        mensagem: ''
+      });
+
+      Swal('Sucesso!', 'O email foi enviado com sucesso.', 'success');
+    } catch (error) {
+      console.error('Erro ao enviar o email:', error);
+
+      Swal('Erro!', 'Ocorreu um erro ao enviar o email. Por favor, tente novamente.', 'error');
+    }
   };
 
   return (
@@ -89,7 +119,7 @@ function Contacts() {
               onChange={handleChange}
             />
 
-            <button disabled={desableBtn}>
+            <button type='submit' disabled={disableBtn}>
               Enviar
             </button>
           </form>
