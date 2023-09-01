62251379ae6a91c35e8e5969c1678bdfe796535c
vitorGaia <vitorgaia0027@gmail.com>
2023-09-01 16:32:39 -0300

    style: timeLine agora responsiva.

--- src/components/timeLine/index.jsx
+++ src/components/timeLine/index.jsx
@@ -3,20 +3,22 @@ import './styles.css';
 function TimeLine() {
   return (
     <div className='time-line-container'>
+        <span>l
         <div className="container left">
+          <div className="content">
+            <h2>Trybe</h2>
+            <p><i className='bx bx-calendar'></i>2022 - 2023</p>
+            <span className='description'>Desenvolvedor Full stack</span> 
+          </div>
+        </div>
+        <div className="container right">
           <div className="content">
             <h2>Unifatecie</h2>
             <p><i className='bx bx-calendar'></i>2021 - 2026</p>
             <span className='description'>Engenharia de Software</span>
           </div>
         </div>
-        <div className="container right">
-          <div className="content">
-            <h2>Trybe</h2>
-            <p><i className='bx bx-calendar'></i>2022 - 2023</p>
-            <span className='description'>Desenvolvedor Full stack</span>
-          </div>
-        </div>
+        </span>
       </div>
   );
 }
